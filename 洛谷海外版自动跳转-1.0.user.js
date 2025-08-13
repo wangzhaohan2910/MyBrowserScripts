672443421fd7ce367d18ecc1dbf2cde1aa159f51
wangzhaohan2910 <158981244+wangzhaohan2910@users.noreply.github.com>
2025-08-13 20:08:50 +0800

    Update 洛谷海外版自动跳转-1.0.user.js

--- 洛谷海外版自动跳转-1.0.user.js
+++ 洛谷海外版自动跳转-1.0.user.js
@@ -10,5 +10,4 @@ var url = window.location.href;
 if (url.startsWith("https://www.luogu.com/article/"))
   window.location.href = url.replace("https://www.luogu.com/article/", "https://www.luogu.me/article/");
 if (url.startsWith("https://www.luogu.com/paste/"))
-
   window.location.href = url.replace("https://www.luogu.com/paste/", "https://www.luogu.me/paste/");
