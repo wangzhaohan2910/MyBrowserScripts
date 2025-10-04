4033d8a617151141e80d29ebe8c5e61184a9dae4
wangzhaohan2910 <158981244+wangzhaohan2910@users.noreply.github.com>
2025-10-04 11:46:44 +0800

    Update and rename 梦熊网站优化-1.0.0.user.js to SYZOJ 网站优化-1.0.0.user.js

--- SYZOJ 网站优化-1.0.0.user.js
+++ SYZOJ 网站优化-1.0.0.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name 梦熊网站优化
-// @description 此脚本优化了 mna.wang 的比赛排行榜以及提交记录界面。
+// @name SYZOJ 网站优化
+// @description 此脚本优化了 SYZOJ 的比赛排行榜以及提交记录界面。
 // @license The Unlicense
 // @homepageURL https://github.com/wangzhaohan2910
 // @version 1.0.0
 // @author wangzhaohan2910
 // @match https://mna.wang/*
+// @match http://47.92.197.167/*
 // ==/UserScript==
 
 function contestName()
